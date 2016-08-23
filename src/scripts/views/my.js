3e0e620475b753eecc1a3168620faa2ec50a62de
duanning <1295367592@qq.com>
2016-08-23 19:19:45 +0800

    my

--- src/scripts/views/my.js
+++ src/scripts/views/my.js
@@ -2,11 +2,6 @@ var indexMy = require('../tpls/my.string');
 
 SPA.defineView('my', {
   html: indexMy,
-//var myscroll; 
-//	function loaded(){
-//	setTimeout(function(){
-//	myscroll=new iScroll("wrapper");},100 );
-//	} 
-//	window.addEventListener("load",loaded,false);
+	
 });
  
