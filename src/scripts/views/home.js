91e7008af0f6eee1a50ce50b49d03598bf6ec720
huoxibin <872627534@qq.com>
2016-08-24 19:33:33 +0800

    home huo

--- src/scripts/views/home.js
+++ src/scripts/views/home.js
@@ -5,13 +5,26 @@ SPA.defineView('home', {
   plugins:[{
   	name:"avalon",
   	options:function(vm){
-  		vm.livelist=[];
+  		vm.hhome=[];
+//		vm.isShowLoading=true;
+//		vm.goodGroups = "";//这里一定要定义一下
   	}
   }],
 //绑定视图事件
 bindEvents:{
 	'show':function(){
 		var vm=this.getVM();
+		$.ajax({
+        url: '/myProject/mock/home-huo.json',
+        type: 'get',
+        data: {},
+        success: function (res) {
+        	vm.hhome = res.data.goodGroups.subject.thumbnailUrl;
+        	console.log(vm.hhome)
+        	vm.thumbnailUrl = res.body.data.thumbnailUrl;
+        }
+      })
+		
 		//轮播图头部
 			var mySwiper=new Swiper("#home-container",{
 				loop:false,
@@ -20,14 +33,28 @@ bindEvents:{
 				autoplay: 2000,
 				loop : true,
 			});
-			//轮播图横向
-			var mySwiper= new Swiper('#home-container1',{
-				freeModeMomentum : false,
-			})
 			//轮播图横向1
-			var mySwiper= new Swiper('#home-container2',{
-				freeModeMomentum : false,
-			})
+			var swiper = new Swiper('#home-container1', {
+       // pagination: '.swiper-pagination',
+        slidesPerView: 3.5,
+        //paginationClickable: true,
+        //spaceBetween:10
+   		 });
+			//轮播图横向2
+			var swiper = new Swiper('#home-container2', {
+       // pagination: '.swiper-pagination',
+        slidesPerView: 3.5,
+        //paginationClickable: true,
+        //spaceBetween:10
+   		 });
+   		 //轮播图横向3
+   		 var swiper = new Swiper('#home-container3', {
+       // pagination: '.swiper-pagination',
+        slidesPerView: 3.5,
+        //paginationClickable: true,
+        //spaceBetween:10
+   		 });
+   		 
 			//点击事件;
 			$(".hxb-home img").on("tap",function(){
 				window.location.href="/myProject/index.html#list#"
