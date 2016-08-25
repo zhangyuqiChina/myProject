0d28288b4bdc4c14575edd496a1357a7495ca46e
huoxibin <872627534@qq.com>
2016-08-25 19:32:00 +0800

    home

--- src/scripts/views/home.js
+++ src/scripts/views/home.js
@@ -21,9 +21,11 @@ bindEvents:{
         success: function (res) {
         	vm.homeCarousels = res.body.carousels;
         	vm.homeGoodgroups= res.body.goodGroups;
-      		vm.homeGoodgroups1= res.body.goodGroups[0].slideGoods;
+        	
+      		vm.homeGoodgroups1= res.body.goodGroups[0].slideGoods;//遍历数组
+      		
 //      	vm.homeSlideGoods= res.body.goodGroups.slideGoods;
-        	console.log(res.body.goodGroups[0].slideGoods)
+        	console.log(res.body.goodGroups[1].slideGoods)
         	
         	
         							//轮播图头部
@@ -36,10 +38,7 @@ bindEvents:{
 											});
 											//轮播图横向1
 											var swiper = new Swiper('#home-container1', {
-								       // pagination: '.swiper-pagination',
 								        slidesPerView: 3.5,
-								        //paginationClickable: true,
-								        //spaceBetween:10
 								   		 });
 											//轮播图横向2
 											var swiper = new Swiper('.home-container2', {
@@ -48,12 +47,12 @@ bindEvents:{
 								        //paginationClickable: true,
 								        //spaceBetween:10
 								   		 });
+								   		//点击事件;
+												$(".hxb-home img").on("tap",function(){
+													window.location.href="/myProject/index.html#list#"
+												})
         }
       })
-			//点击事件;
-			$(".hxb-home img").on("tap",function(){
-				window.location.href="/myProject/index.html#list#"
-			})
 			
 	}
 }
