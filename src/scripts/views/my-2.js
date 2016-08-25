077b8146f637d9ff3eb34dc1120bbf6e9032c642
duanning <1295367592@qq.com>
2016-08-25 19:56:55 +0800

    dd

--- src/scripts/views/my-2.js
+++ src/scripts/views/my-2.js
@@ -2,5 +2,11 @@ var indexMy2 = require('../tpls/my-2.string');
 
 SPA.defineView('my-2', {
   html: indexMy2,
+  bindActions: {
+  	'dBack': function(){
+  		console.log(1);
+  		this.hide();
+  	}
+	}
   
 });
